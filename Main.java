162b00840198c459559c20b09eb9d006878831ea
Angelo <92029180+Anbfer@users.noreply.github.com>
2022-11-22 20:31:44 -0300

    Atualização capítulo 1

--- Main.java
+++ Main.java
@@ -148,17 +148,10 @@ public class Main {
     // -----------------------------
     public static void perguntas() {
         // Nery & Alessandro - Perguntas e história a serem usadas no jogo
-        String[][] perguntas = { {
-                "1 - um zero um zero zero zero um um um um zero (10100011110) - a criatura medonha fez uma pausa e então continuou. - um um um una um zero zero um um zero (1111 1100 110). \n Hawk: Para convertermos um número 'binário' para decimal, basta multiplicarmos essa ordem: 1010 0011 110 pelas potências do número 2. Como por exemplo: 1x210 0x29 1x28 0x27 0x26 0x25 1x24 1x23 1x22 1x21 0x2°. Utilizando a ordem do 1° número binário (10100011110) \nConhece os valores de 2? Para relembrarmos, vou lhe dar uma pequena dica de cálculo: 2x11 = 2 x 2 x 2 x 2 x 2 x 2 x 2 x 2 x 2 x 2 x 2. \n O resultado de uma multiplicação, você multiplica pelo próximo número e assim sucessivamente. Experimente realizar o cálculo e selecionar a alternativa correta:" },
-                { "Hawk: Calma meu (minha) jovem, antes de ficar preocupado deixe-me introduzir este assunto complexo: A ideia da Álgebra Booleana é a de que existem apenas duas teorias em determinada situação/problema, se é VERDADEIRO ou FALSO. Nessa Álgebra podemos encontrar funções e portas lógicas seguidas de suas Tabelas Verdades (que são os verdadeiros valores para determinadas funções, e nunca serão alteradas). As portas lógicas não funcionam apenas com valores numéricos, mas também com circuitos de chaveamento, quando algo está ligado ou desligado, por exemplo.Lamacento: [...] me diga quais afirmativas são verdadeiras e falsas:",
-                        " I. Não, Sim – Resposta óbvia. ", " II. Falso, Verdadeiro - Raciocínio humano.",
-                        " III. True, False – Raciocínio humano composto. ",
-                        " IV. Desligado, Ligado - Circuitos de chaveamento. ", " V. 0, 1 - Sistema binário." }, };
+        String[][] alternativas = {{"(A) 1310 & 2022","(B) 1182 & 2022","(C) 1564 & 2120","(D) 1296 & 1925", "(E) 1309 & 2000"}};
+
         String[][] respostas = {
-                { "(A) 1310 & 2022", "(B) 1182 & 2022", "(C) 1564 & 2120", "(D) 1296 & 1925", "(E) 1309 & 2000" },
-                { "(A) Alternativas II & IV estão corretas.", "(B) Alternativas III & I estão corretas.",
-                        "(C) Somente III & I estão corretas.", "(D) Somente II, IV & V estão corretas.",
-                        "(E) Alternativas I & III estão corretas." } };
+                { "(A) 1310 & 2022"}};
     }
 
     // --------------------PARTE DE
@@ -407,9 +400,60 @@ public class Main {
         tempoPadraoAnimacao((670*10));
         System.out.println("\nuso de no mínimo 5 cristais. lembrando que eu irei lhe ajudar, mas sem lhe dar a resposta correta.");
         tempoPadraoAnimacao((670*10));
-        System.out.println("\n\n\nPreparado %S?");
+        System.out.printf("\n\nO nosso futuro está nas suas mãos %S!", nome_jogador);
+        tempoPadraoAnimacao((500*10));
+        
+        System.out.println("Vocês entram na Florestas das Almas e começam a caminhar");
+        tempoPadraoAnimacao((500*10));
+        System.out.printf(".");
+        tempoPadraoAnimacao((500*10));
+        System.out.printf(".");
+        tempoPadraoAnimacao((500*10));
+        System.out.printf(".\n");
         tempoPadraoAnimacao((670*10));
+        System.out.printf("\nHawk: Há uma coisa que eu não lhe falei, suas escolhas definem o final desta nossa aventura, então tome cuidado no que decidir.");
+        tempoPadraoAnimacao((500*10));
+        System.out.printf("\n\nVocês caminham por mais alguns minutos");
+        tempoPadraoAnimacao((500*10));
+        System.out.printf(".");
+        tempoPadraoAnimacao((500*10));
+        System.out.printf(".");
+        tempoPadraoAnimacao((500*10));
+        System.out.printf(".\n");
+        tempoPadraoAnimacao((500*10));
+        System.out.printf("\nVocê se depara com uma imagem de si mesmo a sua frente, semelhante a um espelho");
+        tempoPadraoAnimacao((670*10));
+        System.out.printf("\nPorém, não se parece com você, é um reflexo acinzentado, com uma aura assustadora e algo marcante ");
+        System.out.printf(".");
+        tempoPadraoAnimacao((500*10));
+        System.out.printf(".");
+        tempoPadraoAnimacao((500*10));
+        System.out.printf(".\n");
+        tempoPadraoAnimacao((500*10));
+        System.out.printf("\nA criatura sussurrava repetidas vezes: \n");
+        tempoPadraoAnimacao((670*10));
+        System.out.printf("-um zero um zero zero zero um um um um zero (10100011110) - a criatura medonha fez uma pausa e então continuou.\n");
+        tempoPadraoAnimacao((500*10));
+        System.out.printf("\n - um um um una um zero zero um um zero (1111 1100 110).\n");
+        tempoPadraoAnimacao((500*10));
+        System.out.printf("Hawk: O que é isso? Ah %S, este é seu primeiro desafio.", nome_jogador);
+        tempoPadraoAnimacao((670*10));
+        System.out.printf("\nHawk: Para convertermos um número 'binário' para decimal, basta multiplicarmos essa ordem: 1010 0011 110 pelas potências do número 2.\n");
+        tempoPadraoAnimacao((670*10));
+        System.out.printf("Como por exemplo: 1x210 0x29 1x28 0x27 0x26 0x25 1x24 1x23 1x22 1x21 0x2°. Utilizando a ordem do 1° número binário (10100011110)\n");
+        tempoPadraoAnimacao((670*10));
+        System.out.printf("Conhece os valores de 2? Para relembrarmos, vou lhe dar uma pequena dica de cálculo: 2x11 = 2 x 2 x 2 x 2 x 2 x 2 x 2 x 2 x 2 x 2 x 2.\n");
+        tempoPadraoAnimacao((670*10));
+        System.out.printf("O resultado de uma multiplicação, você multiplica pelo próximo número e assim sucessivamente. Experimente realizar o cálculo e selecionar a alternativa correta:\n");
+        tempoPadraoAnimacao((670*10));
+        boolean acertou = false;
+        while (!acertou == true) {
+            
+        }
+    }
 
+    public static void capitulo_dois(String nome_jogador) {
+        
     }
 
     //---------------------------- Epilogo --------------------------------------------------
